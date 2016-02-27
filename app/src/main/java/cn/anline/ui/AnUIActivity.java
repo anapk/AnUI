671ef9225a9c9e524fa163c1c992507b56a5fb6e
Jiankian <luozhengan@vip.qq.com>
2016-02-28 04:04:50 +0800

    创建新的AndroidStudio工程
    新建立的Android Studio工程

---
+++ app/src/main/java/cn/anline/ui/AnUIActivity.java
@@ -0,0 +1,17 @@
+package cn.anline.ui;
+//安浪Android UI界面库 索引页
+/*
+作者:jiankian
+QQ
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnUIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_an_ui);
+    }
+}
